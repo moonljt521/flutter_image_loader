c738555ffe5983b09afd8c8eab0dfb30d55c04a0
jiangtao.liang <jiangtao.liang@17zuoye.com>
2020-01-03 14:57:48 +0800

    fix 1.1.2

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/moon/Documents/bak_flutter_sdk/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/moon/Documents/flutter_workspace/flutter_image_loader"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/moon/Documents/bak_flutter_sdk/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=0.0.1"
+export "FLUTTER_BUILD_NUMBER=0.0.1"
